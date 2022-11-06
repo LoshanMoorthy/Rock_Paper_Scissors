a4daf687851f879faa613de0dc6c8e53b1ae2497
Loshan Sundaramoorthy <losh0001@gmail.com>
2022-11-06 20:35:11 +0100

    Updated version of RPS.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * @Author Loshan Sundaramoorthy
+ * @Version 1.1
+ */
+
 import java.util.*;
 import java.io.*;
 
@@ -5,13 +10,35 @@ public class Main {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) throws InterruptedException {
+        creator();
+        gameLoop();
+    }
+
+    static boolean whoWins(String RPS, String AI) {
+        if(RPS.equals("Rock")) {
+            return AI.equals("Scissors");
+        } else if(RPS.equals("Paper")) {
+            return AI.equals("Rock");
+        } else {
+            return AI.equals("Paper");
+        }
+    }
+
+    public static void creator() {
         System.out.println(ANSI_GREEN + "By Loshan Sundaramoorthy - Version 1" + ANSI_RESET);
+    }
+
+    public static void gameLoop() throws InterruptedException {
         Scanner s = new Scanner(System.in);
         String playY = "Yes";
         String playN = "No";
         Random random = new Random();
         System.out.println("Do you wanna play rock, paper or scissors? [Yes or No]");
         String YorN = s.nextLine();
+
+        if(playN.equals(YorN)) {
+            System.exit(0);
+        }
 
         while(playY.equals(YorN)) {
             System.out.println("Awesome");
@@ -20,10 +47,8 @@ public class Main {
             String RPS = s.nextLine();
             Thread.sleep(100);
             System.out.println("You choose " + RPS);
-
             Thread.sleep(1000);
             System.out.print("...");
-
 
             int x = random.nextInt(1,3);
             String AI;
@@ -34,6 +59,7 @@ public class Main {
             } else {
                 AI = "Scissor";
             }
+
             System.out.println("Enemy AI choose " + AI);
 
             if(RPS.equals(AI)) {
@@ -53,19 +79,6 @@ public class Main {
             } else if(again.equals(playY)) {
                 continue;
             }
-        }
-
-        if(playN.equals(YorN)) {
-            System.exit(0);
-        }
-    }
-    static boolean whoWins(String RPS, String AI) {
-        if(RPS.equals("Rock")) {
-            return AI.equals("Scissors");
-        } else if(RPS.equals("Paper")) {
-            return AI.equals("Rock");
-        } else {
-            return AI.equals("Paper");
         }
     }
 }
